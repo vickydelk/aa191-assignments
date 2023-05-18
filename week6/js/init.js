59d5b9425129ff2cb063090ffcda37fa47eba3f7
Victoria Delk <vdelkpictures@gmail.com>
2023-05-18 09:15:54 -0700

    lab for week 7

---
+++ week6/js/init.js
@@ -0,0 +1,50 @@
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function addMarker(data){
+    // console.log(data)
+    // these are the names of our lat/long fields in the google sheets:
+    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data['Where do you live ']}</h2> <h3>${data['What type of pet did\\do you have ']}</h3>`)
+    createButtons(data.lat,data.lng,data['Where do you live '])
+    return
+}
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    const spaceForButtons = document.getElementById('placeForButtons')
+    spaceForButtons.appendChild(newButton);//this adds the button to our page.
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT2tvAe_iDsPUXLMl7pYBOBacBaHGu7Cu9gAXQpbREof15gnaBDrQdWsTps4qzEebDStBV57b3BzWTB/pub?output=tsv"
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
+    })
+}
+
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data)
+    })
+}
+
+loadData(dataUrl)
